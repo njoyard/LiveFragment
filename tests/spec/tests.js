1f3f3fc2eb1a0458249b7f03c2f09ef4f9f97e66
Nicolas Joyard <joyard.nicolas@gmail.com>
2013-03-12 18:40:28 +0100

    Add appendChild and insertBefore tests

--- tests/spec/tests.js
+++ tests/spec/tests.js
@@ -35,6 +35,42 @@ define(['livefragment'], function(LiveFragment) {
 		};
 	};
 
+	
+	var checkNodeRelations = function(fragment, node, position, previous, next) {
+		// Check hasChildNodes()
+		expect( fragment.hasChildNodes() ).toBe( true );
+
+		// Check childNodes, firstChild, lastChild
+		expect( fragment.childNodes[position] ).toBe( node );
+
+		if (!previous) {
+			expect( fragment.firstChild ).toBe( node );
+		}
+
+		if (!next) {
+			expect( fragment.lastChild ).toBe( node );
+		}
+
+		// Check relations with siblings 
+		if (previous) {
+			expect( previous.nextSibling ).toBe( node );
+		} else if (fragment.previousSibling) {
+			expect( fragment.previousSibling.nextSibling ).toBe( node );
+		}
+
+		if (next) {
+			expect( next.previousSibling ).toBe( node );
+		} else if (fragment.nextSibling) {
+			expect( fragment.nextSibling.previousSibling ).toBe( node );
+		}
+		expect( node.previousSibling ).toBe( previous || fragment.previousSibling );
+		expect( node.nextSibling ).toBe( next || fragment.nextSibling );
+
+		// Check relations with parent node
+		expect( node.parentNode ).toBe( fragment.parentNode );
+	};
+
+		
 	return {
 		"LiveFragment creation": function() {
 			expect( createFragment(this) instanceof LiveFragment ).toBe( true );
@@ -81,6 +117,101 @@ define(['livefragment'], function(LiveFragment) {
 					expect( actual[i] ).toBe( expected[i] );
 				}
 			}
+		},
+
+		"LiveFragment#appendChild(new node)": function() {
+			var fragment = createFragment(this),
+				node = document.createElement("div"),
+				prev = fragment.lastChild,
+				children = fragment.childNodes.length,
+				result;
+
+			node.className = "appended";
+			result = fragment.appendChild(node);
+
+			expect( result ).toBe( node );
+			checkNodeRelations(fragment, node, children, prev, null);
+
+			node.parentNode.removeChild(node);
+		},
+
+		"LiveFragment#appendChild(node already in document)": function() {
+			var fragment = createFragment(this),
+				node = document.createElement("div"),
+				prev = fragment.lastChild,
+				children = fragment.childNodes.length,
+				result;
+
+			node.className = "appended";
+
+			document.body.appendChild(node);
+			result = fragment.appendChild(node);
+
+			expect( result ).toBe( node );
+			checkNodeRelations(fragment, node, children, prev, null);
+
+			node.parentNode.removeChild(node);
+		},
+
+		"LiveFragment#insertBefore(new node)": function() {
+			var fragment = createFragment(this),
+				node = document.createElement("div"),
+				next = fragment.lastChild,
+				children = fragment.childNodes.length,
+				prev = children > 1 ? next.previousSibling : null,
+				result;
+
+			node.className = "appended";
+
+			result = fragment.insertBefore(node, next);
+			
+			expect( result ).toBe( node );
+			checkNodeRelations(fragment, node, children > 0 ? children - 1 : 0, prev, next);
+
+			node.parentNode.removeChild(node);
+		},
+
+		"LiveFragment#insertBefore(node already in document)": function() {
+			var fragment = createFragment(this),
+				node = document.createElement("div"),
+				next = fragment.lastChild,
+				children = fragment.childNodes.length,
+				prev = children > 1 ? next.previousSibling : null,
+				result;
+
+			node.className = "appended";
+
+			document.body.appendChild(node);
+			result = fragment.insertBefore(node, next);
+			
+			expect( result ).toBe( node );
+			checkNodeRelations(fragment, node, children > 0 ? children - 1 : 0, prev, next);
+
+			node.parentNode.removeChild(node);
+		},
+
+		"LiveFragment#insertBefore(unknown reference node) throws DOMException 8": function() {
+			var fragment = createFragment("this"),
+				node = document.createElement("div"),
+				ref = document.createElement("div"),
+				exc;
+
+			node.className = "appended";
+			ref.className = "reference";
+
+			document.body.appendChild(ref);
+			try {
+				fragment.insertBefore(node, ref);
+			} catch(e) {
+				exc = e;
+			}
+
+			expect( exc instanceof DOMException ).toBe( true );
+			expect( exc.code ).toBe( 8 );
+			expect( exc.name ).toBe( "NotFoundError" );
+
+			ref.parentNode.removeChild(ref);
 		}
+
 	};
 });
